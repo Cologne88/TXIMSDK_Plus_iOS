71b7a7905d00c5bd0cac9be83df69bc3b2b9d577
yiliangwang <yiliangwang@tencent.com>
2024-10-15 14:35:22 +0800

    package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ImSDK_Plus",
-            targets: ["ImSDK_Plus"]),
+            targets: ["ImSDK_Plus_xcframework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
